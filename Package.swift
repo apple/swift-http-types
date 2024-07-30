716728627ffd636c146817f005b3990286ac53aa
Kevin Herro <kvherro@gmail.com>
2024-07-30 15:26:23 -0700

    Convert HTTPTypesTests and HTTPTypesFoundationTests to Swift Testing framework

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "swift-http-types",
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "HTTPTypes", targets: ["HTTPTypes"]),
         .library(name: "HTTPTypesFoundation", targets: ["HTTPTypesFoundation"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-testing.git", from: "0.11.0"),
     ],
     targets: [
         .target(name: "HTTPTypes"),
@@ -23,12 +25,14 @@ let package = Package(
             name: "HTTPTypesTests",
             dependencies: [
                 "HTTPTypes",
+                .product(name: "Testing", package: "swift-testing"),
             ]
         ),
         .testTarget(
             name: "HTTPTypesFoundationTests",
             dependencies: [
                 "HTTPTypesFoundation",
+                .product(name: "Testing", package: "swift-testing"),
             ]
         ),
     ]
