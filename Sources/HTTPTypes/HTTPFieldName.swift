ac36555f3d712f6d866d0108431baf7bf6990281
Xyan Bhatnagar <x_bhatnagar@apple.com>
2026-05-14 13:49:57 -0700

    Add `Use-As-Dictionary` and sort

--- Sources/HTTPTypes/HTTPFieldName.swift
+++ Sources/HTTPTypes/HTTPFieldName.swift
@@ -254,6 +254,11 @@ extension HTTPField.Name {
     /// https://www.rfc-editor.org/rfc/rfc9110.html
     public static var authorization: Self { .init(rawName: "Authorization", canonicalName: "authorization") }
 
+    /// Available-Dictionary
+    ///
+    /// https://www.rfc-editor.org/rfc/rfc9842.html
+    public static var availableDictionary: Self { .init(rawName: "Available-Dictionary", canonicalName: "available-dictionary") }
+
     /// Cache-Control
     ///
     /// https://www.rfc-editor.org/rfc/rfc9111.html
@@ -331,6 +336,11 @@ extension HTTPField.Name {
     ///
     /// https://www.rfc-editor.org/rfc/rfc9110.html
     public static var date: Self { .init(rawName: "Date", canonicalName: "date") }
+
+    /// Dictionary-ID
+    ///
+    /// https://www.rfc-editor.org/rfc/rfc9842.html
+    public static var dictionaryId: Self { .init(rawName: "Dictionary-ID", canonicalName: "dictionary-id") }
 
     /// Early-Data
     ///
@@ -531,6 +541,11 @@ extension HTTPField.Name {
     /// https://www.rfc-editor.org/rfc/rfc9110.html
     public static var upgrade: Self { .init(rawName: "Upgrade", canonicalName: "upgrade") }
 
+    /// Use-As-Dictionary
+    ///
+    /// https://www.rfc-editor.org/rfc/rfc9842.html
+    public static var useAsDictionary: Self { .init(rawName: "Use-As-Dictionary", canonicalName: "use-as-dictionary") }
+
     /// User-Agent
     ///
     /// https://www.rfc-editor.org/rfc/rfc9110.html
@@ -557,15 +572,4 @@ extension HTTPField.Name {
     public static var xContentTypeOptions: Self {
         .init(rawName: "X-Content-Type-Options", canonicalName: "x-content-type-options")
     }
-
-    /// Available-Dictionary
-    ///
-    /// https://www.rfc-editor.org/rfc/rfc9842.html
-    public static var availableDictionary: Self { .init(rawName: "Available-Dictionary", canonicalName: "available-dictionary") }
-
-    /// Dictionary-ID
-    ///
-    /// https://www.rfc-editor.org/rfc/rfc9842.html
-    public static var dictionaryId: Self { .init(rawName: "Dictionary-ID", canonicalName: "dictionary-id") }
-
 }
